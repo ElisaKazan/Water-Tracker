9a18ed5f14ba1f81838fd4dece89e8eef83631d7
Elisa Kazan <elisa.kazan@gmail.com>
2018-07-28 17:38:12 -0400

    Initial Commit

---
+++ Water TrackerTests/Water_TrackerTests.swift
@@ -0,0 +1,36 @@
+//
+//  Water_TrackerTests.swift
+//  Water TrackerTests
+//
+//  Created by Elisa Kazan on 2018-07-28.
+//  Copyright © 2018 ElisaKazan. All rights reserved.
+//
+
+import XCTest
+@testable import Water_Tracker
+
+class Water_TrackerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
